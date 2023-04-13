6619ad4cfac4e1c98e895fbb5e1bec8adad1aaed
Sam-Cordry <samcordry@gmail.com>
2023-04-12 23:32:01 -0400

    Daily Update 4/12/23

--- Calculus I/scene.py
+++ Calculus I/scene.py
@@ -1,8 +1,9 @@
 from manim import *
+from math import *
 
 class Intro(Scene):
     def construct(self):
-        top_text = Tex("Welcome to", font_size=72).shift(1.5 * UP)
+        top_text = Tex("Welcome to", font_size=72).shift(UP)
         middle_text = Tex("Calculus I", font_size=72).next_to(top_text, DOWN)
         bottom_text = Tex("with Sam Cordry (@samc)", font_size=32).next_to(middle_text, 4 * DOWN)
 
@@ -12,13 +13,14 @@ class Intro(Scene):
         derivative2_text = Tex("\( f^\prime(x) = 2x \)", font_size=48).next_to(derivative1_text, DOWN)
 
         left_axes = Axes(
-            x_range=[-6, 6, 2],
-            y_range=[-6, 6, 2],
-            axis_config={"include_numbers": True, "tip_shape": StealthTip}
+            x_range=[-4, 4, 2],
+            y_range=[0, 6, 2],
+            axis_config={"include_numbers": True, "tip_shape": StealthTip},
+            x_length = 3,
+            y_length = 2,
+            tips=False
         ).shift(5 * LEFT).shift(2 * DOWN)
-        left_axes.width = 5
-        left_graph = left_axes.plot(lambda x: x ** 2, x_range=[-2, 2], use_smoothing=False)
-
+        left_graph = left_axes.plot(lambda x: x ** 2, x_range=[-sqrt(6), sqrt(6)], use_smoothing=True)
 
         # right_axes = Axes()
 
